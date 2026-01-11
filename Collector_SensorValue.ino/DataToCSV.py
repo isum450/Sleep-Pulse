33e64a133f96a07d5c5b85cef368887831fab3cc
leejuhwan1231 <joon0425247@skuniv.ac.kr>
2026-01-11 16:39:45 +0900

    두번쨰

--- Collector_SensorValue.ino/DataToCSV.py
+++ Collector_SensorValue.ino/DataToCSV.py
@@ -6,7 +6,7 @@ ser = serial.Serial('COM4', 115200)#포트
 with open('sensor_datalog.csv', 'a', newline ='') as f:
     writer = csv.writer(f)
     while True:
-        data = ser.readline().decode().strip()
+        data = ser.readline().decode().strip().split(',')
         timestamp = datetime.now().strftime("%Y-%m-%d %H: %M: %S")
         writer.writerow([timestamp, data])
         print(f"{timestamp}, {data}")
