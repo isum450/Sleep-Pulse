dac63707b6c8fc597579020f1d000cebc3eb1c0e
isum450 <132076877+isum450@users.noreply.github.com>
2026-01-17 17:32:35 +0900

    feat/mqtt protocol 적용

--- Collector_SensorValue.ino/Collector_SensorValue.ino.ino
+++ Collector_SensorValue.ino/Collector_SensorValue.ino.ino
@@ -1,8 +1,9 @@
+#include<WiFi.h>
 #include<Wire.h>
 #include "DHT.h"
-#include "FS.h"
-#include "SD_MMC.h"
 #include<PubSubClient.h>
+#include <ArduinoJson.h>
+#include <math.h>
 
 #define DHTPIN 33
 #define DHTTPE DHT11
@@ -10,9 +11,9 @@
 DHT dht(DHTPIN, DHTTPE);
 
 const int MPU=0x68;//MPU6050 I2C주소
-const char* ssid = "";//Wifi ssid
-const char* password = "";//Wifi password
-const char* mqtt_server = ""; //브로커 IP 주소
+const char* ssid = "U+Net7428";//Wifi ssid
+const char* password = "1J3A4AF87#";//Wifi password
+const char* mqtt_server = "broker.hivemq.com"; //wifi IP 주소
 
 WiFiClient espClient;
 PubSubClient client(espClient);
@@ -55,24 +56,12 @@ void callback(char* topic, byte* payload, unsigned int length)
   Serial.println();
 }
 
-void callback(char* topic, byte* payload, unsigned int length)
-{
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for(int i = 0; i < length; i++)
-  {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-}
-
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("ArduinoClient")) {
       Serial.println("connected");
-      client.subscribe("test/topic");
+      client.subscribe("leesu/sensor/data");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -85,6 +74,8 @@ void reconnect() {
 void setup() {
   setup_wifi();
   client.setServer(mqtt_server, 1883);
+  client.setSocketTimeout(60); 
+  client.setKeepAlive(60);
   client.setCallback(callback);
   Wire.begin(27, 26);
   Wire.beginTransmission(MPU);
@@ -99,19 +90,27 @@ void loop() {
   if(!client.connected()) {
     reconnect();
   }
-    
+  client.loop();
+
   get6050();//센서값 갱신
   //받아온 센서값을 출력
-  float VectorMove = abs(pow(AcX,2) + pow(AcY, 2) + pow(AcZ, 2));
+  float VectorMove = sqrt(pow(AcX,2) + pow(AcY, 2) + pow(AcZ, 2));
   float h = dht.readHumidity();    // 습도
   float t = dht.readTemperature(); // 온도(섭씨)
   int illuminanceValue = analogRead(32); //조도센서 값 측정
 
-  client.publish("sensor/VectorMove", String(VectorMove).c_str());
-  client.publish("sensor/humidity", String(h).c_str());
-  client.publish("sensor/temperature", String(t).c_str());
-  client.publish("sensor/illuminance", String(illuminanceValue).c_str());
-/*
+  JsonDocument doc;//JSON형식으로 데이터를 전송하기 위해서
+  doc["motion"] = VectorMove;
+  doc["humidity"] = h;
+  doc["temperature"] = t;
+  doc["illuminance"] = illuminanceValue;
+  char buffer[256];
+  serializeJson(doc, buffer);
+  client.publish("leesu/sensor/data", buffer);
+
+  Serial.println(buffer);
+
+ /*
   Serial.print(VectorMove);
   Serial.print(",");
   Serial.print(h);
@@ -120,8 +119,7 @@ void loop() {
   Serial.print(",");
   Serial.println(illuminanceValue);      //조도센서 값 출력
 */
-  client.loop();
-  delay(1000);
+  delay(2000);
 }
 
 void get6050(){
@@ -137,8 +135,5 @@ void get6050(){
     GyX = Wire.read() << 8 | Wire.read();
     GyY = Wire.read() << 8 | Wire.read();
     GyZ = Wire.read() << 8 | Wire.read();
-  } else {
-    // 읽어오지 못했을 때의 처리 (디버깅용)
-    // Serial.println("No data from sensor");
-  }
+  } 
 }
