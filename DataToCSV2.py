e8b0a02ede5a2fc1bf12e67afb10503c0120f171
leejuhwan1231 <joon0425247@skuniv.ac.kr>
2026-01-18 17:29:02 +0900

    Update DataToCSV2.py

--- DataToCSV2.py
+++ DataToCSV2.py
@@ -3,14 +3,32 @@ import csv
 import time
 import statistics
 from datetime import datetime
+import json
+import paho.mqtt.client as mqtt
 
 ser = serial.Serial('COM4', 115200)
 FILENAME = 'sensor_datalog.csv'
+broker_address = "broker.hivemq.com"
+topic = "leesu/sensor/data"
+
+try:
+    # Paho MQTT v2.x 대응
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+except AttributeError:
+    # Paho MQTT v1.x 대응
+    client = mqtt.Client()
+
+try:
+    print(f"브로커({broker_address}) 연결 시도 중...")
+    client.connect(broker_address, 1883, 60)
+    client.loop_forever()
+except KeyboardInterrupt:
+    print("\n프로그램 종료")
 
 buffer_hum = []
 buffer_temp = []
 buffer_lux = []
-buffer_move = []
+buffer_motion = []
 
 try:
     with open(FILENAME, 'x', newline='') as f:
@@ -25,19 +43,19 @@ try:
             if not line: continue
 
             try:
-                parts = line,split(',')
-                if len(parts) >= 4:
-                    m = int(parts[0])
-                    h = float(parts[1])
-                    t = float(parts[2])
-                    l = int(parts[3])
+                data = json.loads(line)
+                m = float(data.get("motion", 0))  # 아두이노의 "motion"
+                h = float(data.get("humidity", 0))
+                t = float(data.get("temperature", 0))
+                l = int(data.get("illuminance", 0))
 
-                    buffer_hum.append(h)
-                    buffer_temp.append(t)
-                    buffer_lux.append(l)
+                buffer_hum.append(h)
+                buffer_temp.append(t)
+                buffer_lux.append(l)
 
-                    print("움직임{m}, 습도{h}%, 온도{t}, 조도{3}")
-
+                print(f"움직임{m}, 습도{h}%, 온도{t}, 조도{l}")
+            except json.JSONDecodeError:
+                continue
             except ValueError:
                 continue
 
@@ -58,7 +76,8 @@ try:
             buffer_temp.clear()
             buffer_lux.clear()
 
-            time.sleep(0.01)
+        time.sleep(0.01)
 
 except KeyboardInterrupt:
     ser.close()
+    client.disconnect()
